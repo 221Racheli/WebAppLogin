f26f9046f4d14b5e521ce5c57938e8b3fb8cba33
325593952 <325593952@PC1-11>
2023-02-23 18:09:24 +0200

    Add project files.

---
+++ WebAppLoginEx1/Controllers/usersController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.PortableExecutable;
+using System.Text.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAppLoginEx1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usersController : ControllerBase
+    {
+
+        string filePath = "./usersDetails.txt";
+
+        // GET: api/<usersController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<usersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+
+        // POST api/<usersController>
+
+
+        [HttpPost]
+        public ActionResult<User> LoginPost([FromBody] User loginUser)
+        {
+            User found = foundUser(loginUser);
+            if (found != null)
+                return found;
+            return NoContent();
+        }
+
+
+
+        [HttpPost("regist")]
+        public ActionResult<User> Post([FromBody] User userRegist)
+        {
+            if (!existUserName(userRegist.email))
+            {
+                Console.WriteLine("regist controler");
+                int numberOfUsers = System.IO.File.ReadLines(filePath).Count();
+                userRegist.userId = numberOfUsers + 1;
+                string userJson = JsonSerializer.Serialize(userRegist);
+                System.IO.File.AppendAllText(filePath, userJson + Environment.NewLine);
+                return CreatedAtAction(nameof(Get), new { id = userRegist.userId }, userRegist);
+            }
+            return BadRequest("email already exists");
+
+        }
+
+
+        // PUT api/<usersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] User userToUpdate)
+        {
+            string textToReplace = string.Empty;
+            using (StreamReader reader = System.IO.File.OpenText(filePath))
+            {
+                string currentUserInFile;
+                while ((currentUserInFile = reader.ReadLine()) != null)
+                {
+
+                    User user = JsonSerializer.Deserialize<User>(currentUserInFile);
+                    if (user.userId == id)
+                        textToReplace = currentUserInFile;
+                }
+            }
+            if (textToReplace != string.Empty)
+            {
+                string text = System.IO.File.ReadAllText(filePath);
+                text = text.Replace(textToReplace, JsonSerializer.Serialize(userToUpdate));
+                System.IO.File.WriteAllText(filePath, text);
+            }
+
+        }
+
+        // DELETE api/<usersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public User foundUser(User userToSearch)
+        {
+            using (StreamReader reader = System.IO.File.OpenText(filePath))
+            {
+                string? currentUserInFile;
+                while ((currentUserInFile = reader.ReadLine()) != null)
+                {
+                    User user = JsonSerializer.Deserialize<User>(currentUserInFile);
+                    if (user.email == userToSearch.email && user.password == userToSearch.password)
+                        return user;
+                }
+            }
+            return null;
+        }
+
+        public Boolean existUserName(String userName)
+        {
+            using (StreamReader reader = System.IO.File.OpenText(filePath))
+            {
+                string? currentUserInFile;
+                while ((currentUserInFile = reader.ReadLine()) != null)
+                {
+                    User user = JsonSerializer.Deserialize<User>(currentUserInFile);
+                    if (user.email == userName)
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ WebAppLoginEx1/wwwroot/home.js
@@ -0,0 +1,69 @@
+﻿
+
+async function LogIn() {
+    console.log("login");
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const user = JSON.stringify({ password: password, email: email });
+    const response = await fetch(
+        'https://localhost:44390/api/users', {
+        method: 'POST',
+        body: user,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    );
+    if (!response.ok) {
+        alert("user name or email not exists");
+    }
+    else {
+        if (response.status == '204') {
+            alert("user name or email not exists");
+        }
+        else {
+            const data = await response.json();
+            if (data) {
+                sessionStorage.setItem("userInfo", JSON.stringify({ firstName: data.firstName, lastName: data.lastName }));
+                sessionStorage.setItem("firstName", data.firstName);
+                sessionStorage.setItem("id", data.userId);
+                window.location.href = "update.html";
+            }
+        }
+    }
+}
+
+
+async function Regist() {
+    const email = document.getElementById("registEmail").value;
+    const password = document.getElementById("registPassword").value;
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const user = JSON.stringify({ password : password, email: email, firstName: firstName, lastName: lastName });
+    console.log("hello");
+    console.log(email);
+    console.log(password);
+    const response = await fetch(
+        'https://localhost:44390/api/users/regist', {
+        method: 'POST',
+        body: user,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    );
+    if (!response.ok) {
+        if (response.status == '400') {
+            alert(`email already exists ${response.status}`);
+        }  
+    }
+    else {
+        if (response.status == '201') {
+            alert("user created");
+        }
+        else {
+            alert("all fields are require");
+        }
+    }
+}
+
